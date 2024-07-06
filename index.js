253d5438037c12479367949b9cf4e26a46f4dea3
Denning <gichabadenning254@gmail.com>
2024-07-06 11:24:28 +0300

    writes functions and paerses the value to a string or number

---
+++ index.js
@@ -0,0 +1,35 @@
+//adds 2 numbers
+function add(a,b){
+     return a+b;
+}
+
+//subtracts 2 numbers
+function subtract(a,b){
+    return a-b;
+}
+
+//multipies 2 numbers
+function multiply(a,b){
+    return a*b;
+}
+
+//divides a number with another
+function divide(a,b){
+    return a/b;
+}
+
+function increment(n) {
+    return n + 1;
+}
+
+function decrement(n) {
+    return n - 1;
+}
+
+function makeInt(string){
+    return parseInt(string, 10)
+}
+
+function preserveDecimal(string){
+    return parseFloat(string)
+}
